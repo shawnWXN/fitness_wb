f8f89bd7c65a1fbef27394170ee16da9e065384e
shawn_wxn <shawn_wxn@163.com>
2024-03-19 13:41:05 +0800

    feat: courses接口支持id搜索

--- src/orm/course_orm.py
+++ src/orm/course_orm.py
@@ -9,20 +9,19 @@ async def find_courses(request) -> dict:
     """
     课程列表
     """
-    # _id: str = request.args.get(CONST.ID)
-    # search: str = request.args.get(CONST.SEARCH)
-    # query = CourseModel.filter()
-    # if _id:
-    #     query = query.filter(id=_id)
-    # else:
-    #     if search:
-    #         query = query.filter(Q(name__icontains=search) | Q(coach_name__icontains=search))
-    #
-    # return await paging(request, query)
-
+    _id: str = request.args.get(CONST.ID)
     search: str = request.args.get(CONST.SEARCH)
     query = CourseModel.filter()
-    if search:
+    if _id:
+        query = query.filter(id=_id)
+    elif search:
         query = query.filter(Q(name__icontains=search) | Q(coach_name__icontains=search))
 
     return await paging(request, query)
+
+    # search: str = request.args.get(CONST.SEARCH)
+    # query = CourseModel.filter()
+    # if search:
+    #     query = query.filter(Q(name__icontains=search) | Q(coach_name__icontains=search))
+
+    # return await paging(request, query)
