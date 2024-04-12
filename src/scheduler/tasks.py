cb830112127a5916e19cb16dd373841c8d656ccf
shawn <18874488665@163.com>
2024-04-12 21:54:05 +0800

    test: 测试apscheduler

--- src/scheduler/tasks.py
+++ src/scheduler/tasks.py
@@ -1,14 +1,14 @@
 import time
 from datetime import datetime, timedelta
 
+from loggers.logger import logger
 from scheduler.core import aps
 
 
 def func1():
-    print(time.time())
-    aps.add_job(func1, 'date', run_date=datetime.now() + timedelta(seconds=3))
+    logger.info(time.time())
+    aps.add_job(func1, 'date', run_date=datetime.now() + timedelta(seconds=30))
 
 
 def run_tasks():
-    ...
-    # aps.add_job(func1, 'date', run_date=datetime.now() + timedelta(seconds=3))
+    aps.add_job(func1, 'date', run_date=datetime.now() + timedelta(seconds=30))
