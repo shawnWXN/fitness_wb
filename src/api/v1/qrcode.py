90a1f1590e77cea32f46d1b3310d947a5d024680
shawn_wxn <shawn_wxn@163.com>
2024-04-20 14:07:20 +0800

    test: 增加日志输出

--- src/api/v1/qrcode.py
+++ src/api/v1/qrcode.py
@@ -8,6 +8,7 @@ from sanic.views import HTTPMethodView
 from api import check_staff, check_authorize
 from common.enum import StaffRoleEnum, ScanSceneEnum, OrderStatusEnum
 from infra.utils import resp_failure, resp_success, str2base64
+from loggers.logger import logger
 from orm.model import OrderModel, UserModel, ExpenseModel
 from service.validate_service import validate_qrcode_create_data
 
@@ -42,7 +43,9 @@ class Qrcode(HTTPMethodView):
             member: UserModel = await UserModel.get_one(openid=_id)
             assert member.id == request.ctx.user.id, f"OrderModel[{_id}] no access for user[{request.ctx.user.id}]"
 
+        logger.info(f"start {qrcode_str} -> {time.time_ns()}")
         qrcode_storage[qrcode_str] = time.time_ns()
+        logger.info(f"now qrcode_storage {qrcode_storage}")
         return resp_success(qrcode=str2base64(qrcode_str))
 
     @staticmethod
@@ -58,7 +61,9 @@ class Qrcode(HTTPMethodView):
             return resp_failure(400, "缺少必要参数")
 
         # 判断有无过期
+        logger.info(f"now qrcode_storage {qrcode_storage}")
         ts: typing.Optional[float, None] = qrcode_storage.pop(scene_uuid, None)
+        logger.info(f"pop qrcode_storage get {ts}")
         if not ts or (ts + qrcode_expire_ts * pow(10, 9)) < time.time_ns():
             return resp_failure(500, "二维码已失效")
 
