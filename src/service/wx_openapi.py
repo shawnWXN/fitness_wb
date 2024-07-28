503a1af82ae03d1dd5d581c9fa1dd3f1c8109427
shawn_wxn <shawn_wxn@163.com>
2024-07-28 18:40:35 +0800

    test: add msg_proxy interface

--- src/service/wx_openapi.py
+++ src/service/wx_openapi.py
@@ -26,3 +26,55 @@ def phone_via_code(code: str) -> str:
     resp_dict = response.json()
     assert resp_dict.get('phone_info', {}).get('phoneNumber'), str(resp_dict)
     return resp_dict.get('phone_info', {}).get('phoneNumber')
+
+
+def send_api(openid, template_id):
+    url = "http://api.weixin.qq.com/cgi-bin/message/subscribe/send"
+
+    if template_id == 'ErC-bxEn78ABZRpHb_oDhobYc7yOVBEaj7MJBK5akLk':
+        payload = {
+            "touser": openid,
+            "template_id": template_id,
+            "miniprogram_state": "developer",
+            "data": {
+                "thing1": {
+                    "value": "三个月包时卡",
+                },
+                "time3": {
+                    "value": "2022年4月26日 21:48",
+                },
+                "time2": {
+                    "value": "2024年8月26日 21:48",
+                },
+            },
+        }
+    # i7Ib4M_rAMJ8NABZUqO1Y8Oqqm71NLzrOzAZXypMajA
+    else:
+        payload = {
+            "touser": openid,
+            "template_id": template_id,
+            "miniprogram_state": "developer",
+            "data": {
+                "thing1": {
+                    "value": "套餐类型1",
+                },
+                "number3": {
+                    "value": 52,
+                },
+                "number2": {
+                    "value": 3
+                },
+                "thing5": {
+                    "value": "这是一个提醒"
+                }
+            },
+        }
+
+    try:
+        response: requests.Response = requests_retry('post', url, json=payload)
+        response.raise_for_status()  # Raise an error for bad responses
+        print(f'send_api, Response[{response.status_code}] -> {response.text}')
+        return response.json()  # Return the JSON response
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
